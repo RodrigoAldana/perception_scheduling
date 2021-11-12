53504db974a92b157fd503fc910b182d3ed6b52e
RodrigoAldana <rodrigo.aldana.lopez@gmail.com>
2021-11-12 10:14:28 +0100

    First version. Only constructs covariance graph

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from viz_utils import plot2d_order2
 
 
 sim = TargetSim(skip_sim=50)
-pcov = np.array([0.01]) 
+pcov = np.array([0.01])
 sim.add_target(number=1, order=2, ws_dim=2, pcov=pcov)
 sim.set_random_x0()
 
-h = 0.001
+h = 0.001 
 deltas = [0.1]
 mcovs = [np.array([0.001])]
 
